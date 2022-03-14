db1f422638a2a1e95dd6872193f4d0cc06852863
whiteewolf <arog0660@gmail.com>
2022-03-14 22:18:35 +0200

    partly done setup command

--- src/Commands/setup.js
+++ src/Commands/setup.js
@@ -14,9 +14,30 @@ module.exports = {
      * 
      * @param {CommandInteraction} interaction 
      */
-    run: (interaction) => {
-        interaction.reply({content: 'test'})
-        const data = guild.findOne({ guildID: interaction.guild.id}, async (data) => {
+    run: async (interaction) => {
+        const data = guild.findOne({ guildID: interaction.guild.id})
+      const filter = (m) => {
+          return m.author.id === interaction.user.id;
+      }
+      const collector = await interaction.channel.createMessageCollector({
+          filter,
+          max: 1,
+        time: 30000
+      });
+      interaction.reply({embeds: [new MessageEmbed().setColor("ORANGE").setTitle('Setup your server').setDescription('Please provide a category ID where to create your tickets!\n> You have 30 seconds\n\nType \`cancel\` to cancel setup')]})
+      collector.on("collect", (collected) => {
+          console.log(collected)
+          if(collected.content.toLowerCase() === `cancel`) {
+              collector.stop("User cancelled...");
+              return interaction.channel.send({content: 'Cancelled setup...'});
+          } else {
+              if(!interaction.guild.channels.cache.get(collected.content)) return interaction.channel.send({content: 'Please provide valid category ID!'})
+              if(data) {
+                  data.TicketCategoryID = collected.content;
+                //   data.save();
+              }
+            }
+        })
             if(!data) {
                 new guild({
                     guildID: interaction.guild.id,
@@ -27,6 +48,5 @@ module.exports = {
                 interaction.fetchReply();
                 interaction.channel.send({content: 'Created schema'})
             }
-        });
     }
 }
